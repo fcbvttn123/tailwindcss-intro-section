703864ba9a620754abd824d4ff1029c4b3e7569b
ngan <vutran@sheridancollege.ca>
2024-04-29 18:19:16 -0400

    Backdrop added to the Dialog

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,8 +19,9 @@ export function Header() {
             </div>
 
             {/* Dialog (headlessui) */}
-            <Dialog open={isOpen} onClose={() => setIsOpen(false)} className={"absolute inset-0 left-32 bg-red-500 px-6 pt-16"}>
-                <Dialog.Panel>
+            <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+                <div className="fixed inset-0 bg-black/30 -z-10" aria-hidden="true" />
+                <Dialog.Panel className={"bg-white absolute inset-0 left-32 px-6 pt-16"}>
                     <div className="flex flex-col items-start gap-y-4">
                         <button className="flex items-center justify-between min-w-28">
                             <span className="sr-only">Navigation Link: Features</span>
@@ -39,7 +40,7 @@ export function Header() {
                         <button className="text-slate-600 font-semibold text-lg">Login</button>
                         <button className="text-slate-600 font-semibold text-lg border-2 border-slate-600 rounded-2xl w-full py-2">Register</button>
                     </div>
-                    <button>
+                    <button onClick={() => setIsOpen(false)}>
                         <span className="sr-only">Close Dialog</span>
                         <XMarkIcon className="w-10 h-10 text-slate-600 absolute right-4 top-4" aria-hidden="true"/>
                     </button>
