50b728b8a3d1a7c3331c1bdeafbf9c12a89d4092
ngan <vutran@sheridancollege.ca>
2024-04-28 21:57:01 -0400

    Header Update: HTML and CSS is done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,16 @@
+import { Bars4Icon } from "@heroicons/react/24/outline"
+
 export function Header() {
     return (
-        <div>
-            <div><img src="./images/logo.svg" alt="" srcSet="" /></div>
-            <div></div>
+        <div className="flex items-center justify-between max-w-96 mx-auto py-4">
+            <button>
+                <img src="./images/logo.svg" aria-hidden="true"/>
+                <span className="sr-only">Logo</span>
+            </button>
+            <button>
+                <Bars4Icon className="w-6 h-6" aria-hidden="true"/>
+                <span className="sr-only">Menu Button</span>
+            </button>
         </div>
     )
 }
