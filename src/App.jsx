fff4889798f1bc1e44c7f829baaa5d723f45b976
ngan <vutran@sheridancollege.ca>
2024-04-28 21:48:55 -0400

    'heroicons' library added and 'Header' component is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
 
 function App() {
-  return <h1 className="text-red-500 font-bold text-xl">Hello world!</h1>
+  return (
+    <Header />
+  )
 }
 
 export default App
